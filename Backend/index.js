a9efef50eb2532db813376f4597858e75d514b81
Pratik Dayma <pratikdayma45@gamil.com>
2024-07-16 21:38:35 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,101 @@
+// import express from 'express';
+// import { nanoid } from 'nanoid';
+// import fs from 'fs';
+// import cors from 'cors';
+
+// const app = express();
+// var corsOptions = {
+//     origin: 'http://localhost:5173/',
+//     methods:'POST,GET',
+//     credentials:true,
+//   }
+// app.use(cors(corsOptions));
+// app.use(express.json());
+
+// app.post('/', (req, res) => {
+//     const shortUrl = nanoid(8);
+//     const urlsFromFile = fs.readFileSync('urls.json');
+//     const urlsJson = JSON.parse(urlsFromFile.toString());
+//     urlsJson[shortUrl] = req.body.longUrl;
+//     fs.writeFileSync('urls.json', JSON.stringify(urlsJson));
+//     res.json({
+//         status: true,
+//         data: `http://localhost:10000/${shortUrl}`,
+//         message: 'Short URL generated'
+//     });
+// });
+
+// app.get('/:short', (req, res) => {
+//     const shortUrl = req.params.short;
+//     const urlsFromFile = fs.readFileSync('urls.json');
+//     const urlsJson = JSON.parse(urlsFromFile.toString());
+
+//     const longUrl = urlsJson[shortUrl];
+//     if (longUrl) {
+//         res.redirect(longUrl);
+//     } else {
+//         res.status(404).json({
+//             status: false,
+//             message: 'Short URL not found'
+//         });
+//     }
+// });
+
+// app.listen(10000, () => {
+//     console.log('Server is running on port 10000');
+// });
+
+
+
+import express from 'express';
+import { nanoid } from 'nanoid';
+import fs from 'fs';
+import cors from 'cors';
+
+const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'POST,GET',
+    credentials: true,
+}));
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    if(!req.body.longUrl){
+       res.status(400).json({
+        status:false,
+        message:"Enter the long URl"
+       })
+       return;
+    }
+    const shortUrl = nanoid(8);
+    const urlsFromFile = fs.readFileSync('urls.json');
+    const urlsJson = JSON.parse(urlsFromFile.toString());
+    urlsJson[shortUrl] = req.body.longUrl;
+    fs.writeFileSync('urls.json', JSON.stringify(urlsJson));
+    res.json({
+        status: true,
+        data: `http://localhost:10000/${shortUrl}`,
+        message: 'Short URL generated'
+    });
+});
+
+app.get('/:short', (req, res) => {
+    const shortUrl = req.params.short;
+    const urlsFromFile = fs.readFileSync('urls.json');
+    const urlsJson = JSON.parse(urlsFromFile.toString());
+
+    const longUrl = urlsJson[shortUrl];
+    if (longUrl) {
+        res.redirect(longUrl);
+    } else {
+        res.status(404).json({
+            status: false,
+            message: 'Short URL not found'
+        });
+    }
+});
+
+app.listen(10000, () => {
+    console.log('Server is running on port 10000');
+});
---
+++ Frontend/Url_Shortner_App/src/Components/Form.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+
+ function Form() {
+    const [longUrlObj,setLongUrlObj]=useState({
+        longUrl:"",
+    });
+    const[shortUrl,setShortUrl]=useState('');
+    const changeIp=(e)=>{
+          setLongUrlObj({longUrl:e.target.value});
+    }
+  async function getShortUrlFunction(e){
+    e.preventDefault();
+   if(!longUrlObj.longUrl){
+    alert("Please Enter Long URl ....")
+   }
+   try {
+    const response=await fetch('http://localhost:10000/',{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body:JSON.stringify(longUrlObj)
+    });
+    console.log(response);
+    const data = await response.json();
+    setShortUrl(data.data);
+   } catch (error) {
+    console.log("Error Occured while fetching data" ,error);
+   }
+
+   }
+  return (
+   <main className="h-[100vh] w-[100vw] flex justify-center items-center text-[25px] flex-col " >
+       <form action="" className="shadow-[0px_0px_5px_black] w-[60%] text-center p-2 flex flex-col gap-4 mb-6">
+        <input type="text" placeholder="Enter the Long URL...." className="w-[100%] px-2 py-1 border-2 border-black" 
+        onChange={changeIp}/>
+        <button className="bg-blue-400 px-4 py-2 w-fit mx-auto" onClick={getShortUrlFunction}>Get Url</button>
+       </form>
+
+       {
+        shortUrl &&
+        <div className=" text-[25px] p-4 shadow-[0px_0px_5px_black] w-[50%]">
+            <h1 className="my-2">Your shorten URL is here ....</h1>
+            <a className="my-2 text-blue-600 underline text-[20px]" target="blank" href={shortUrl}>{shortUrl}</a>
+        </div>
+        
+       }
+     </main>
+  )
+}
+
+export default Form;
